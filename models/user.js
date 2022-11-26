c2c8b5faaf81aae3fe90ebbc6cda8293c20833e7
mayankkuhaad <117817131+mayankkuhaad@users.noreply.github.com>
2022-11-26 16:22:54 +0530

    user schema updated

--- models/user.js
+++ models/user.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-    name: { type: String },
-    email: { type: String , unique:true},
-    phone: { type: Number , unique:true},
-    state: { type: String },
-    district: { type: String },
-    address: { type: String },
-    pincode: { type: String },
+    name: { type: String , required: true},
+    email: { type: String , unique:true, required: true},
+    phone: { type: Number , unique:true, required: true},
+    state: { type: String, required: true },
+    district: { type: String, required: true },
+    address: { type: String, required: true },
+    pincode: { type: String, required: true },
     password: {type: String, required:true}
 })
 
